8f4d9d0e7722eb315e7776694638f5594aadc6eb
Mpachoud <32837324+Mpachoud@users.noreply.github.com>
2021-02-24 08:39:24 +0100

    lines between blocks only on matlab >= 2014

--- wc_plot_features_vs_time.m
+++ wc_plot_features_vs_time.m
@@ -18,15 +18,17 @@ colind = [];
 zeroind = max(interv);
 
 total_N_spikes= numel([handles.classind{:}]);
-if ~isfield(handles,'blocksamplesperchannel') % During
+
+if ~isfield(handles,'blocksamplesperchannel') % to plot lines between blocks 
     load([handles.pathname 'concatenation_info.mat'],'blocksamplesperchannel');
     handles.blocksamplesperchannel = blocksamplesperchannel;
 end
+
 for k=interv
-%     max_spikes = min(MAX_SPIKES_TO_PLOT,length(handles.classind{k}));
+    %     max_spikes = min(MAX_SPIKES_TO_PLOT,length(handles.classind{k}));
     max_spikes=round(length(handles.classind{k})/total_N_spikes*MAX_SPIKES_TO_PLOT);
     spk_indexes=randperm(length(handles.classind{k}));
-    toplotind = cat(2,toplotind,handles.classind{k}(spk_indexes(1:max_spikes)));    
+    toplotind = cat(2,toplotind,handles.classind{k}(spk_indexes(1:max_spikes)));
     if k == zeroind &&  handles.ncl<k
         colind = cat(1,colind,zeros(max_spikes,3));
     else
@@ -55,11 +57,11 @@ mnx=prctile(handles.features(:,1),0.1)*1.1;
 bins=[min(handles.index(toplotind)) max(handles.index(toplotind))];
 bins=[bins(1):diff(bins)/100:bins(2)-diff(bins)/200]+diff(bins)/200;
 
-    histo=hist(handles.index,bins);
-    max_all=max(histo);
-    histo=histo/max_all*(mxx-mnx);
-    histo=histo+mnx;
-    plot(cax,histo,bins,'k:','linewidth',2);
+histo=hist(handles.index,bins);
+max_all=max(histo);
+histo=histo/max_all*(mxx-mnx);
+histo=histo+mnx;
+plot(cax,histo,bins,'k:','linewidth',2);
 for k=interv
     histo=hist(handles.index(handles.classind{k}),bins);
     histo=histo/max_all*(mxx-mnx);
@@ -71,7 +73,7 @@ for i=2:nf
     %for j=i+1:nf
     cax=handles.htaxes(i);
     cla(cax);hold(cax,'on');
-        
+    
     if VER>=2014 %since this part only works for matlab 2014 +
         hLine = scatter(cax,handles.features(toplotind,i),handles.index(toplotind),1,colind(:,1:3),'o','filled');
         hLine.MarkerHandle.get;
@@ -81,22 +83,24 @@ for i=2:nf
         hLine.MarkerHandle.FaceColorBinding = 'discrete';
         hLine.MarkerHandle.FaceColorData = uint8(255*colind)';
         hLine.UserData = {1 i};
+        
+        %draw horizontal lines between blocks
+        if size(handles.blocksamplesperchannel{1,handles.channel},1) > 1
+            for n_blocks = 2:size(handles.blocksamplesperchannel{1,handles.channel},1)
+                block_x_vector = linspace(min(get(cax,'Xlim')),max(get(cax,'Xlim')),100);
+                block_y_vector = repmat((handles.blocksamplesperchannel{1,handles.channel}(n_blocks,1)/handles.WC.sr)*1000, 1, length(block_x_vector));
+                hLine2 = scatter(cax, block_x_vector, block_y_vector);
+                hLine2.LineWidth = 0.1;
+                hLine2.MarkerEdgeColor = 'k';
+                hLine2.Marker = '.';
+            end
+            clear hLine2
+        end
     else
-        [~, colix]=ismember(colind(:,1:3),handles.colors(used_colors,:),'rows');        
+        [~, colix]=ismember(colind(:,1:3),handles.colors(used_colors,:),'rows');
         hLine = scatter(cax,handles.features(toplotind,i),handles.index(toplotind),15,colix,'.');
         set(hLine,'UserData',{i 1});
     end
-    %draw horizontal lines between blocks
-    if size(handles.blocksamplesperchannel{1,handles.channel},1) > 1
-        for n_blocks = 2:size(handles.blocksamplesperchannel{1,handles.channel},1)
-            block_x_vector = linspace(min(get(cax,'Xlim')),max(get(cax,'Xlim')),100);
-            block_y_vector = repmat((handles.blocksamplesperchannel{1,handles.channel}(n_blocks,1)/handles.WC.sr)*1000, 1, length(block_x_vector));
-            hLine2 = scatter(cax, block_x_vector, block_y_vector);
-            hLine2.LineWidth = 0.1;
-            hLine2.MarkerEdgeColor = 'k';
-            hLine2.Marker = '.';
-        end
-        clear hLine2
-    end
+    
     clear hLine
 end
